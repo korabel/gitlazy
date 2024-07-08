2c55d19332266a9856a82bc8ee5d7d666784d9f6
Michael Korabelnikov <korabel@gmail.com>
2024-07-07 20:53:02 -0400

    Add tests for GitDriver.

---
+++ tests/git_driver_tests.py
@@ -0,0 +1,12 @@
+from unittest.mock import MagicMock
+from drivers.git import GitDriver
+
+
+def test_init_git_repo():
+    pass
+
+def test_get_current_diff():
+    pass
+
+def test_commit_with_comments():
+    pass
